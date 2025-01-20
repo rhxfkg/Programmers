96effc2e8a50f9a596c0d4f6c7b353f2cce683ff
rhxfkg <rhxfkg@gmail.com>
2025-01-20 14:47:59 +0900

    Update 다리를 지나는 트럭 해결

--- src/level2/truck_crossing_bridge/Solution.java
+++ src/level2/truck_crossing_bridge/Solution.java
@@ -8,42 +8,44 @@ public class Solution {
         int answer = 1;
         Queue<Integer> bridge = new LinkedList<>();
 
-        //7,4,5,6
-        //7 -> 2초
-        //4,5 -> 2초+1초 = 3초
-        //6 -> 2초
-        //2+3+2+1 = 8
-
-        //10
-        //100초 + 1초
-
-        //10,10,10,10,10,10,10,10,10,10
-        //100초 + 9초 + 1초 = 110초
-
-        //먼저 큐에 bridge_length 이하까지 담은 것을 하나로 취급하고 length만큼 시간을 추가함
-        //구성 요소 - 1만큼 시간 초를 추가하고 기본 1초를 추가함
-        for (int i = 0; i < truck_weights.length; i++) {
+        int[] count = new int[truck_weights.length];
+        int start = 0;
+        int index = 0;
+        int sum = 0;
+        while (true) {
+            answer++;
             if (bridge.isEmpty()) {
-                bridge.offer(truck_weights[i]);
-            } else if (weightsOnBridge(bridge) + truck_weights[i] <= weight) {
-                bridge.offer(truck_weights[i]);
+                bridge.offer(truck_weights[index]);
+                sum += truck_weights[index];
+                count[index]++;
+                index++;
             } else {
-                answer += bridge_length + bridge.size() - 1;
-                bridge.clear();
-                bridge.offer(truck_weights[i]);
+                if (index < truck_weights.length && sum + truck_weights[index] <= weight) {
+                    bridge.offer(truck_weights[index]);
+                    sum += truck_weights[index];
+                    index++;
+                    for (int i = start; i < index; i++) {
+                        count[i]++;
+                        if (count[i] == bridge_length) {
+                            sum -= bridge.peek();
+                            bridge.poll();
+                            start++;
+                        }
+                    }
+                } else {
+                    for (int i = start; i < index; i++) {
+                        count[i]++;
+                        if (count[i] == bridge_length) {
+                            sum -= bridge.peek();
+                            bridge.poll();
+                            start++;
+                        }
+                    }
+                }
             }
+            if (index == truck_weights.length && bridge.isEmpty()) break;
         }
-        answer += bridge_length + bridge.size() - 1;
 
         return answer;
-    }
-
-    private static int weightsOnBridge(Queue<Integer> bridge) {
-        int sum = 0;
-        for (Integer i : bridge) {
-            sum += i;
-        }
-
-        return sum;
     }
 }
